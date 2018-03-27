6e8350b3fc09d5ba0948e38d0c94f5642628ff1c
Ram Mahajan <rammahajan7@gmail.com>
2018-03-27 12:48:36 +0800

    added types parameter in query string

--- server/routes/google-autocomplete.js
+++ server/routes/google-autocomplete.js
@@ -5,11 +5,18 @@ const config = require('../config');
 
 router.get('/', function(req, res, next) {
   let mockData;
-  let tflURI = `${config.google_autocomplete_url}?input=${
-    req.query.input
-  }&types=establishment&location=${req.query.location}&radius=500&key=${
-    config.google_app_key
-  }`;
+  let tflURI;
+  if (req.query.types === 'geocode') {
+    tflURI = `${config.google_autocomplete_url}?input=${
+      req.query.input
+    }&types=${req.query.types}&language=en&key=${config.google_app_key}`;
+  } else {
+    tflURI = `${config.google_autocomplete_url}?input=${
+      req.query.input
+    }&types=${req.query.types}&location=${req.query.location}&radius=500&key=${
+      config.google_app_key
+    }`;
+  }
   if (config.is_mock_data) {
     return res.status(200).send(mockData);
   }
