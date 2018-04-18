016e940c1716d510736bc98afaaefd79e7db44ff
Beda Tse <beda@coeus.hk>
2018-04-18 18:40:57 +0800

    remove sensitive config from source repository

--- server/config.js
+++ server/config.js
@@ -2,11 +2,11 @@ module.exports = {
   bike_api_url: 'https://api.tfl.gov.uk/BikePoint',
   bike_search_api_url: 'https://api.tfl.gov.uk/BikePoint/Search',
   bike_occupancy_api_url: 'https://api.tfl.gov.uk/Occupancy/BikePoints',
-  endpointBaseUrl: process.env.ENDPOINT_BASEINTERFACE || 'localhost:3000',
   google_autocomplete_url:
     'https://maps.googleapis.com/maps/api/place/autocomplete/json',
-  app_id: '5ebfd576',
-  bike_app_key: '019b1eb8b5ddd58804fe1cb884ce009b',
-  google_app_key: 'AIzaSyCW99Wo8aZ4Qu73I6owTrPsGrXe3s5u0CM',
+  endpointBaseUrl: process.env.ENDPOINT_BASEINTERFACE || 'localhost:3000',   
+  app_id: process.env.BIKE_APP_ID || '',
+  bike_app_key: process.env.BIKE_APP_KEY || '',
+  google_app_key: process.env.GOOGLE_APP_KEY || '',
   is_mock_data: false
 };
